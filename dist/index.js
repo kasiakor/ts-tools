b5f3c3c4e64be25a5e90930b12f44241bdbf9667
kasia.kordys <kasia.kordys@gmail.com>
2021-07-07 20:18:47 +0100

    test support for map() in es5

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
 var printMessage = function (msg) { return console.log("Message: " + msg); };
 var message = ("Hello from TypeScript");
 printMessage(message);
+var data = new Map();
+data.set("Bob", "London");
+data.set("Alice", "Paris");
+data.forEach(function (val, key) { return console.log(val + " lives in " + key); });
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ let printMessage= (msg: string): void => console.log(`Message: ${msg}`);
 
 let message = ("Hello from TypeScript");
 printMessage(message);
+
+let data = new Map();
+data.set("Bob", "London");
+data.set("Alice", "Paris");
+data.forEach((val, key) => console.log(`${val} lives in ${key}`));
